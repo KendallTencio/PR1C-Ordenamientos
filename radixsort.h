4e0ef5af3f1ff4ff3f70d8a02ba09bb5da605fba
KendallTencio <kendalltencio132e@gmail.com>
2018-09-22 14:36:02 -0600

    Radixsort
    Se añade el radixsort como plantilla

--- radixsort.h
+++ radixsort.h
@@ -1,4 +1,20 @@
 #ifndef RADIXSORT_H
 #define RADIXSORT_H
 
+class RadixSort
+{
+private:
+    int x = 0;
+    int vectorRS[20];
+public:
+    RadixSort();
+    template <class T>
+    void radixsort(T *vectorRS, int n);
+    template <class T>
+    void countSort(T *arr[], int n, int exp);
+     template <class T>
+    int getMax(T *arr[], int n);
+};
+
 #endif // RADIXSORT_H
+
