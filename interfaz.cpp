64352ad4d276ca0a2467d9637fdf3dee11182100
KendallTencio <kendalltencio132e@gmail.com>
2018-09-24 21:48:52 -0600

    main

--- interfaz.cpp
+++ interfaz.cpp
@@ -19,33 +19,6 @@ Interfaz::~Interfaz()
 
 void Interfaz::on_pushButton_clicked()
 {
-      srand(time(0));
-      int i;
-      int vector[20];
-      int vectorOBJ[20];
-
-      //char vectorC[] = {'q','t','s','u','c','k','s'};
-
-      //------------------------Bubble Bidireccional--------------------------------------------
-      BIBubbleSort burbujaBiPrueba;
-      for (i = 0; i<19; i++){
-          vector[i] = (1 + rand() % 100);
-      }
-      cout<<"Lista desordenada con el Bubble Bidireccional: "<<endl;
-      cout<<"\n";
-      for (i = 0; i<19; i++){
-          cout<<vector[i];
-          cout<<", ";
-      }
-      cout<<"\n"<<endl;
-      burbujaBiPrueba.bubbleBi_sort(vector);
-      cout<<"Lista ordenada con el Bubble Bidireccional: "<<endl;
-      cout<<"\n";
-      for (i = 0; i<19; i++){
-          cout<<vector[i];
-          cout<<", ";
-      }
-      cout<<"\n"<<endl;
 
 
 }
--- main.cpp
+++ main.cpp
@@ -1,21 +1,18 @@
 #include "interfaz.h"
 #include <QApplication>
+#include "pila.cpp"
 #include <iostream>
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     Interfaz w;
     w.show();
-<<<<<<< HEAD
     ListaEnlazada<int> i;
     i.insertarFinal(12);
     i.insertarFinal(321);
     i.insertarFinal(7843);
-    i.getEliminarPos(1);
+    i.EliminarPos(1);
     std::cout << i.getPos(0)->getElemento() << std::endl;
-=======
-
->>>>>>> 9a242a98ed1a202a815f5e5babd1c77145980327
 
 
 
