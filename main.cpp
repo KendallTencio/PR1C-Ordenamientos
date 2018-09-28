10c53fbc2b216ee5f88c1be7bd8d05c62c889dfb
KendallTencio <kendalltencio132e@gmail.com>
2018-09-28 16:48:24 -0600

    .

--- main.cpp
+++ main.cpp
@@ -12,18 +12,14 @@ int main(int argc, char *argv[])
     //MainWindow w;
     //w.show();
     //return a.exec();
-    cout<<"HOla"<<endl;
     ListaEnlazada<int> b;
     b.insertarCabeza(12);
     b.insertarCabeza(7);
-    cout<<"HOla2"<<endl;
   //  BubbleSort<int> a;
     InsertionSort<int> x;
-    cout<<"HOla3"<<endl;
     x.Insertion_sort(&b);
-    cout<<"HOla4"<<endl;
     cout<<b.getCabeza()->getElemento()<<endl;
-    cout<<"HOla5"<<endl;
+
     //a.bubble_sort(&b);
     return 0;
 }
