4ecd70783dd20560d3651a83143eee0a73ebed23
KendallTencio <kendalltencio132e@gmail.com>
2018-09-28 17:15:11 -0600

    Interfaz diseño básico

--- main.cpp
+++ main.cpp
@@ -8,10 +8,10 @@ using namespace std;
 
 int main(int argc, char *argv[])
 {
-    //QApplication a(argc, argv);
-    //MainWindow w;
-    //w.show();
-    //return a.exec();
+    QApplication a(argc, argv);
+    MainWindow w;
+    w.show();
+/*
     ListaEnlazada<int> b;
     b.insertarCabeza(12);
     b.insertarCabeza(7);
@@ -19,7 +19,7 @@ int main(int argc, char *argv[])
     InsertionSort<int> x;
     x.Insertion_sort(&b);
     cout<<b.getCabeza()->getElemento()<<endl;
-
+*/
     //a.bubble_sort(&b);
-    return 0;
+    return a.exec();
 }
