3dac5907564407c44e4d8fab30d93d1f06c26c03
KendallTencio <kendalltencio132e@gmail.com>
2018-09-28 16:28:43 -0600

    insertSort listaEnlazada

--- Ordenamientos/insertion.cpp
+++ Ordenamientos/insertion.cpp
@@ -1,5 +1,5 @@
 #include "insertion.h"
-#include "Estructuras/listaenlazada.h"
+#include "Estructuras/listaenlazada.cpp"
 
 template <class T>
 InsertionSort<T>::InsertionSort(){
@@ -25,14 +25,14 @@ template <class T>
 void InsertionSort<T>::Insertion_sort(ListaEnlazada<T>* listaEnlazada)
 {
     int i, j;
-    int actual;
+    Nodo<int> *actual;
 
     for (i = 1; i < listaEnlazada->getTam(); i++) {
-        listaEnlazada->insertarPos(listaEnlazada->getPos(i)->getElemento(),actual);
+        actual = listaEnlazada->getPos(i);
         for (j = i; j > 0 /*&& listaEnlazada->getPos(j - 1) > actual*/; j--) {
             listaEnlazada->insertarPos(listaEnlazada->getPos(j - 1)->getElemento(),j);
         }
-        listaEnlazada->insertarPos(listaEnlazada->getPos(actual)->getElemento(),j);
+        listaEnlazada->insertarPos(listaEnlazada->getPos(actual->getElemento())->getElemento(),j);
     }
 }
 
--- main.cpp
+++ main.cpp
@@ -2,19 +2,28 @@
 #include <QApplication>
 #include <iostream>
 //#include "Ordenamientos/bubblesort.cpp"
-#include "Ordenamientos/insertion.h"
+#include "Ordenamientos/insertion.cpp"
+
+using namespace std;
+
 int main(int argc, char *argv[])
 {
     //QApplication a(argc, argv);
     //MainWindow w;
     //w.show();
     //return a.exec();
+    cout<<"HOla"<<endl;
     ListaEnlazada<int> b;
     b.insertarCabeza(12);
     b.insertarCabeza(7);
+    cout<<"HOla2"<<endl;
   //  BubbleSort<int> a;
     InsertionSort<int> x;
+    cout<<"HOla3"<<endl;
     x.Insertion_sort(&b);
+    cout<<"HOla4"<<endl;
+    cout<<b.getCabeza()->getElemento()<<endl;
+    cout<<"HOla5"<<endl;
     //a.bubble_sort(&b);
     return 0;
 }
